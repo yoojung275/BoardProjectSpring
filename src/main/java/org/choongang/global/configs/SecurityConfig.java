bf7d5c712910eccb2d883be5908bd35dbce7480e
yoojung275 <yoojung275@gmail.com>
2024-07-26 14:55:02 +0900

    초기 설정 중

---
+++ src/main/java/org/choongang/global/configs/SecurityConfig.java
@@ -0,0 +1,14 @@
+package org.choongang.global.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.build();
+    }
+}
