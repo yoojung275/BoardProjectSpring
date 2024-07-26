c0bbcbc8983859e824108ba39b761e49ab7e2330
yoojung275 <yoojung275@gmail.com>
2024-07-26 15:23:16 +0900

    초기 설정 중 오류 해결

--- src/main/java/org/choongang/global/configs/SecurityConfig.java
+++ src/main/java/org/choongang/global/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.choongang.global.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -7,6 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http.build();
